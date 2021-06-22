67277dca3dcb909c02f8d5dad7fa6f220240f29b
EduardoAlphonse <eduardo.afoli@gmail.com>
2021-06-22 12:30:13 -0300

    rotas com react router dom, navegação com Link e useHistory, criação do contexto de autenticação, autenticação com o google, criação do hook useAuth

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={Home} />
-      <Route path='/rooms/new' component={NewRoom} />
+      <AuthContextProvider>
+        <Route exact path='/' component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -9,18 +7,18 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
 	const history = useHistory();
+	const { user, signInWithGoogle } = useAuth();
 
-	function handleCreateRoom() {
-		const provider = new firebase.auth.GoogleAuthProvider();
+	async function handleCreateRoom() {
+		if (!user) {
+			await signInWithGoogle();
+		}
 
-		auth.signInWithPopup(provider).then(result => {
-			console.log(result);
-
-			history.push('/rooms/new');
-		});
+		history.push('/rooms/new');
 	}
 
 	return (
