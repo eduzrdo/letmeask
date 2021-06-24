af174d1fdabda320d6b8c32763048fb2a9c799f1
EduardoAlphonse <eduardo.afoli@gmail.com>
2021-06-24 15:36:01 -0300

    reorganizando a páginas em pastas próprias para si e seus estilos, e criação de nova página AdminRoom

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import googleIconImg from '../../assets/images/google-icon.svg';
 
-import { database } from '../services/firebase';
+import { database } from '../../services/firebase';
 
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
+import { Button } from '../../components/Button';
+import { useAuth } from '../../hooks/useAuth';
 
-import '../styles/auth.scss';
+import './styles.scss';
 
 export function Home() {
 	const history = useHistory();
@@ -36,6 +36,11 @@ export function Home() {
 
 		if (!roomRef.exists()) {
 			alert('Room does not exist.');
+			return;
+		}
+
+		if (roomRef.val().endedAt) {
+			alert('Room already closed.');
 			return;
 		}
 
