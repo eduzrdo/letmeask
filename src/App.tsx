dd1a8f571d76f232b1bd4e2004b022992d930921
EduardoAlphonse <eduardo.afoli@gmail.com>
2021-06-24 15:36:18 -0300

    adicionando nova rota AdminRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route path='/rooms/new' component={NewRoom} />
           <Route path='/rooms/:id' component={Room} />
+          <Route path='/admin/rooms/:id' component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
